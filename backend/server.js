8a5d0588584f805217d8fb6bceb4142ab676e648
Subin Chaliyath <subinchaliyath@gmail.com>
2021-01-29 18:19:13 +0530

    redux added

--- backend/server.js
+++ backend/server.js
@@ -19,5 +19,5 @@ app.use(notFound);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`server started at ${PORT}`));
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,28 +1,39 @@
-import React, {useState, useEffect} from "react";
+import React, { useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/productActions";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 import { Product } from "../components/Product";
-import axios from 'axios';
-export const HomeScreen = () => {
-const [products, SetProducts] =useState([])
 
-useEffect(() => {
-  const fetchProducts= async()=>{
-    const {data}=await axios.get('/api/products');
-    SetProducts(data)
-  }
-  fetchProducts()
-}, [])
+export const HomeScreen = () => {
+  const dispatch = useDispatch();
+
+  const productList = useSelector((state) => state.productList);
+  const { products, error, loading } = productList;
+
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <>
-      <h1>Laest Products</h1>
-      <Row>
-        {products.map((product) => {
-          return <Col  key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product}></Product>
-          </Col>
-        })}
-      </Row>
+      <h1>Latest Products</h1>
+      {loading ? (
+        <Loader/>
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row>
+          {products.map((product) => {
+            return (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product}></Product>
+              </Col>
+            );
+          })}
+        </Row>
+      )}
     </>
   );
 };
