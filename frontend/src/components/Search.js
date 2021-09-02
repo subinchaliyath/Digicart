1fc184978c6b8e9a0e49e6b051c80e1c13a5adcf
subin <subinchaliyath@gmail.com>
2021-09-02 23:07:17 +0530

    top margin for button in mobile screen

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -22,7 +22,7 @@ export const Search = ({history}) => {
         value={keyword}
         onChange={(e) => setKeyword(e.target.value)}
       ></Form.Control>
-      <Button type="submit" variant="outline-success" className='p-2 mt-sm-1'>
+      <Button type="submit" variant="outline-success" className='p-2 mt-2 mt-sm-0'>
         Search
       </Button>
     </Form>
